612d8d1acd9ea994f362be0013ad416359b6506b
lucas <UO277440@uniovi.es>
2023-03-07 11:42:23 +0100

    LAB 5 -> version final.

--- src/main/java/es/uniovi/dlp/ast/expression/ArithmeticOperation.java
+++ src/main/java/es/uniovi/dlp/ast/expression/ArithmeticOperation.java
@@ -8,5 +8,8 @@ public class ArithmeticOperation extends AbstractExpression {
   public ArithmeticOperation(
       int line, int column, Expression leftExpression, String op, Expression rightExpression) {
     super(line, column);
+    this.leftExpression=leftExpression;
+    this.rightExpression=rightExpression;
+    this.op=op;
   }
 }
